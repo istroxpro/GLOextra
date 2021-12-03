c1c3dcb269680a2901ebf288ce847e0d537fb839
istroxpro <istrodantir@icloud.com>
2021-12-03 16:34:46 +0700

    Усложненное задание к четвертому уроку

--- extra.js
+++ extra.js
@@ -1,38 +1,14 @@
-// Вариант через многомерный массив
+let dummy = prompt('Введите строку');
 
-let Day = (dayNumber, lang) => ({
-    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-})[lang][dayNumber % 7];
-console.log(Day(3, 'ru'));
-console.log(Day(6, 'en'));
+const extraFunc = function (d) {
 
-// Вариант через условную конструкцию if
+    if (typeof d !== 'string') {
+        alert('аргумент не строка');
+        return;
+    }
 
-let language = 'ru';
-let dayArr;
-if (language == 'ru') {
-    dayArr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-} else if (language == 'en') {
-    dayArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    d = d.trim();
+    alert(d.length > 30 ? d.slice(0, 30) + '...' : d);
 };
-console.log(dayArr)
 
-// Вариант через switch-case
-
-let switchLang = 'en';
-let switchArr;
-switch (switchLang) {
-    case 'ru':
-        switchArr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-        break;
-    case 'en':
-        switchArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-        break;
-}
-console.log(switchArr);
-
-// Задание №2
-let namePerson = 'Артем';
-let personStatus = namePerson == 'Артем' ? 'Директор' : namePerson == 'Александр' ? 'Преподаватель' : 'Студент';
-console.log(personStatus);
+extraFunc(dummy);
